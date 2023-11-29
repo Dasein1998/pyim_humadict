7e05ca80b37766e0e3875ebf5e88dabd12f7eaff
Dasein1998 <42430560+Dasein1998@users.noreply.github.com>
2023-11-29 22:52:43 +0800

    update

--- pyim-humadict.el
+++ pyim-humadict.el
@@ -1,12 +1,11 @@
 (require 'pyim)
 (pyim-scheme-add
     '(huma
-      :document
-      "虎码方案"
+      :document "虎码方案"
       :class xingma
-      :code-prefix "huma/"
       :first-chars "abcdefghijklmnopqrstuvwxyz"
       :rest-chars "abcdefghijklmnopqrstuvwxyz"
+      :code-prefix "huma/"
       :code-prefix-history ("_")
       :code-split-length 4
       :code-maximum-length 4))
