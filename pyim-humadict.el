eff9d14071f36615df1533732d4534d4df3bdcea
Dasein1998 <dasein1998@gmail.com>
2024-05-09 05:32:20 +0800

    auto-insert

--- pyim-humadict.el
+++ pyim-humadict.el
@@ -1,3 +1,29 @@
+;;; pyim-humadict.el --- huma for pyim               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Dasein
+
+;; Author: Dasein <dasein@Dasein-mac.local>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
+(provide 'pyim-humadict)
+;;; pyim-humadict.el ends here
+
 ;;; pyim-humadict.el
 (require 'pyim)
 (pyim-scheme-add
